ef81c9d8b9882ffc9f6bab3da9b7803370fc2112
hellomrleeus <lee_lixin@outlook.com>
2019-05-28 16:51:18 +0800

    add benchmark client

---
+++ client/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash address
+CONNECTIONS=$1
+REPLICAS=$2
+IP=$3
+#go build --tags "static netgo" -o client client.go
+for (( c=0; c<${REPLICAS}; c++ ))
+do
+    docker run -v $(pwd)/client:/client --name 1mclient_$c -d alpine /client \
+    -conn=${CONNECTIONS} -ip=${IP}
+done
